f88b14f5440edcf8d6f3f1c150a0ade2aaa32686
harshalp27 <harshalp28@gmail.com>
2017-08-21 15:08:13 +0530

    use ng-model to display full name
    the ng-model directive to get the first name and last name from the user and displyed using handlebars syntax.

---
+++ Intern/Basics/Exercise3/app.js
@@ -0,0 +1,8 @@
+var app = angular.module("BasicsExeapp3", [])
+app.controller('BasicsExeCtrl3', function($scope){
+                          
+               $scope.firstName = "Hello";
+               $scope.lastName = "Friend";
+                              
+                          
+               });
